cdf23c078a3c26091afa661e2fec25c496660de3
AungZinTun <aungzintun2@gmail.com>
2020-04-30 22:44:27 +0630

    all

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+
+
+    <v-content>
+      <AppNavigation/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import AppNavigation from './components/AppNavigation';
+
+export default {
+  name: 'App',
+
+  components: {
+    AppNavigation,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
